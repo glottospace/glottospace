c7d9bb84f5a9823e6ef4b9b7ee9ecd60a2006a41
Sietze Norder <sietze@sietzenorder.nl>
2021-12-20 09:46:48 +0100

    glottosave: guess filename

--- R/glottosave.R
+++ R/glottosave.R
@@ -30,15 +30,17 @@
 
 glottosave <- function(glottodata, filename = NULL){
 
+  if(is.null(filename)){filename <- deparse(substitute(glottodata))}
+
   if((class(glottodata) == "tmap")[1]){
-    if(tools::file_ext(filename) == ""){
+    if( tools::file_ext(filename) == "" ){
     ifelse(getOption("tmap.mode") == "plot", filename <- paste0(filename, ".png"), filename <- paste0(filename, ".html"))
     }
     tmap::tmap_save(glottodata, filename = filename)
     message(paste0("Map (tmap object) saved as ", filename))
   } else if( is_sf(glottodata) ){
     # if no file extension: gpkg
-    if(tools::file_ext(filename) == ""){
+    if( tools::file_ext(filename) == "" ){
       sf::st_write(obj = glottodata, dsn = paste0(filename, ".gpkg"),
                append = FALSE)
     } else {
@@ -47,14 +49,14 @@ glottosave <- function(glottodata, filename = NULL){
     }
     message(paste0("Spatial object (sf) saved as ", filename))
   } else if(any(class(glottodata) == "matrix" ) ){
-    if(tools::file_ext(filename) == ""){
+    if( tools::file_ext(filename) == "" ){
     utils::write.csv(glottodata, file = paste0(filename, ".csv"))
     } else {
       utils::write.csv(glottodata, file = filename)
     }
     message(paste0("Matrix saved as ", filename))
   } else if(class(glottodata) == "data.frame"){
-    if(tools::file_ext(filename) == ""){filename <- paste0(filename, ".xlsx")}
+    if( tools::file_ext(filename) == "" ){filename <- paste0(filename, ".xlsx")}
     writexl::write_xlsx(glottodata, path = filename) # works better than openxlsx, which omits some columns..
     message(paste0("Data.frame saved as ", filename))
   } else(
