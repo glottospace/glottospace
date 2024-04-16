5690fbaddb2cbb238bc325f7c3a423a5ef43bc6f
ruidongsmile <ruidongsmile@mail.com>
2024-04-16 15:48:05 +0200

    fix some bugs

--- R/glottostat.R
+++ R/glottostat.R
@@ -99,48 +99,6 @@ return(resultsdf)
 #' glottostat_dist_permanova(glottodist = glottodist, glottodata = glottodata, comparison = "pairwise")
 #'
 #'
-# glottostat_dist_permanova <- function(glottodist = NULL, glottodata = NULL, comparison = NULL, sample = NULL, permutations = NULL, by = NULL){
-#
-#   if(is.null(permutations)){permutations <- 999}
-#   if(is.null(comparison)){comparison <- "overall"}
-#   if(is.null(by)){by <- "group"}
-#
-#   if(glottocheck_hassample(glottodata) & is.null(sample)){
-#     glottosample <- glottodata[["sample"]]
-#   } else if(glottocheck_hassample(glottodata) & !is.null(sample)){
-#     message("The glottodata has a sample sheet, but the Permanova is based on the given argument \"sample\".")
-#     glottosample <- sample
-#   } else if(!glottocheck_hassample(glottodata) & !is.null(sample)){
-#     message("The glottodata has no sample sheet, so the Permanova is based on the given argument \"sample\".")
-#     glottosample <- sample
-#   } else if(!glottocheck_hassample(glottodata) & is.null(sample)){
-#     stop("Please provide a sample table.")
-#   }
-#
-#   if("group" %nin% colnames(glottosample)){stop("There is no group column in the sample table. Use glottocreate_sampletable()")}
-#   if(all(is.na(glottosample$group))){stop("Please add groups to the sample table. Use glottocreate_sampletable()")}
-#
-#   id <- glottocheck_id(glottodata)
-#
-#   metadist <- glottojoin_dist(glottodata = glottodata, glottodist = glottodist, na.rm = TRUE)
-#
-#   if(id == "glottosubcode"){
-#     metadist$glottocode <- glottoconvert_subcodes(metadist$glottosubcode)
-#     metadist <- glottojoin_data(glottodata = metadist, with = glottosample, type = "left", id = "glottocode")
-#   } else{
-#     metadist <- glottojoin(glottodata = metadist, with = glottosample, id = "glottocode")
-#   }
-#
-#   if(comparison == "overall"){
-#     message("Running overall permanova")
-#     resultsdf <- glottostat_permanovall(metadist = metadist, id = id, permutations = permutations, by = by)
-#   } else if(comparison == "pairwise"){
-#     message("Running pairwise permanova")
-#     resultsdf <- glottostat_permanovapairs(metadist = metadist, id = id, permutations = permutations, by = by)
-#   } else{stop("Please specify the type of comparison ('overall' or 'pairwise') ")}
-#   return(resultsdf)
-# }
-
 glottostat_dist_permanova <- function(glottodist = NULL, glottodata = NULL, comparison = NULL, sample = NULL, permutations = NULL, by = NULL){
 
   if(is.null(permutations)){permutations <- 999}
@@ -346,3 +304,45 @@ pvalvecstars <- function(pvalvec){
 
   pvalvec
 }
+
+# glottostat_dist_permanova <- function(glottodist = NULL, glottodata = NULL, comparison = NULL, sample = NULL, permutations = NULL, by = NULL){
+#
+#   if(is.null(permutations)){permutations <- 999}
+#   if(is.null(comparison)){comparison <- "overall"}
+#   if(is.null(by)){by <- "group"}
+#
+#   if(glottocheck_hassample(glottodata) & is.null(sample)){
+#     glottosample <- glottodata[["sample"]]
+#   } else if(glottocheck_hassample(glottodata) & !is.null(sample)){
+#     message("The glottodata has a sample sheet, but the Permanova is based on the given argument \"sample\".")
+#     glottosample <- sample
+#   } else if(!glottocheck_hassample(glottodata) & !is.null(sample)){
+#     message("The glottodata has no sample sheet, so the Permanova is based on the given argument \"sample\".")
+#     glottosample <- sample
+#   } else if(!glottocheck_hassample(glottodata) & is.null(sample)){
+#     stop("Please provide a sample table.")
+#   }
+#
+#   if("group" %nin% colnames(glottosample)){stop("There is no group column in the sample table. Use glottocreate_sampletable()")}
+#   if(all(is.na(glottosample$group))){stop("Please add groups to the sample table. Use glottocreate_sampletable()")}
+#
+#   id <- glottocheck_id(glottodata)
+#
+#   metadist <- glottojoin_dist(glottodata = glottodata, glottodist = glottodist, na.rm = TRUE)
+#
+#   if(id == "glottosubcode"){
+#     metadist$glottocode <- glottoconvert_subcodes(metadist$glottosubcode)
+#     metadist <- glottojoin_data(glottodata = metadist, with = glottosample, type = "left", id = "glottocode")
+#   } else{
+#     metadist <- glottojoin(glottodata = metadist, with = glottosample, id = "glottocode")
+#   }
+#
+#   if(comparison == "overall"){
+#     message("Running overall permanova")
+#     resultsdf <- glottostat_permanovall(metadist = metadist, id = id, permutations = permutations, by = by)
+#   } else if(comparison == "pairwise"){
+#     message("Running pairwise permanova")
+#     resultsdf <- glottostat_permanovapairs(metadist = metadist, id = id, permutations = permutations, by = by)
+#   } else{stop("Please specify the type of comparison ('overall' or 'pairwise') ")}
+#   return(resultsdf)
+# }
