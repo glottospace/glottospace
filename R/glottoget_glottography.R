ea45ef42075290a5a2cd766e8d5d70f17b6a9a4c
meroos <meeliroose@gmail.com>
2024-12-14 19:38:05 +0200

    implement commit, test

--- R/glottoget_glottography.R
+++ R/glottoget_glottography.R
@@ -1,4 +1,4 @@
-#' Get glottography data
+#' Get glottography data test
 #'
 #' This function loads the Glottography data that is distributed with glottospace or optionally downloads it.
 #' @noRd
@@ -42,28 +42,28 @@ glottoget_glottographyloadlocal <- function(dirpath){
   cldf_metadata <- base::list.files(dirpath, pattern = "cldf-metadata.json", recursive = TRUE)
   mdpath <- normalizePath(file.path(dirpath, cldf_metadata))
   mddir <- normalizePath(base::dirname(mdpath))
-  
+
   # Load languages file
   languoids <- normalizePath(file.path(mddir, "languages.csv"))
   languoids <- utils::read.csv(languoids, header = TRUE, encoding = "UTF-8")
   colnames(languoids) <- base::tolower(colnames(languoids))
   colnames(languoids)[which(colnames(languoids) == "id")] <- "lang_id"
-  
+
   # Load glottography-specific data file
   values <- normalizePath(file.path(mddir, "glottography_values.csv"))
   values <- utils::read.csv(values, header = TRUE, encoding = "UTF-8")
   colnames(values) <- base::tolower(colnames(values))
   colnames(values)[colnames(values) == "language_id"] <- "lang_id"
   values <- tidyr::pivot_wider(data = values, names_from = "parameter_id", values_from = "value")
-  
+
   # Example for potential levels or categories (modify as needed for glottography-specific fields)
   levels <- values[!is.na(values$level), c("lang_id", "level")]
   category <- values[!is.na(values$category), c("lang_id", "category")]
-  
+
   glottographydata <- dplyr::left_join(languoids, levels, by = "lang_id") %>%
     dplyr::left_join(category, by = "lang_id") %>%
     dplyr::arrange(.data$lang_id)
-  
+
   colnames(glottographydata)[which(colnames(glottographydata) == "lang_id")] <- "id"
   invisible(glottographydata)
 }
