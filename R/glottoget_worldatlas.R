ea5ecef690aff4b2b39ec5b1b242dd9cdaa1a22e
meroos <meeliroose@gmail.com>
2025-06-21 13:39:20 +0300

    Add worldatlas support to glottoget system via Zenodo

--- R/glottoget_worldatlas.R
+++ R/glottoget_worldatlas.R
@@ -10,17 +10,25 @@ glottoget_worldatlas <- function(version = c("contemporary", "traditional"),
                                  polygon_set = c("features", "languages", "families"),
                                  download = FALSE,
                                  dirpath = NULL) {
-
   version <- match.arg(version)
   polygon_set <- match.arg(polygon_set)
 
-  out <- glottoget_zenodo(
-    name = "worldatlas",
-    subfolder = file.path("cldf", version),
-    file = paste0(polygon_set, ".geojson"),
-    download = download,
-    dirpath = dirpath
-  )
+  if (download) {
+    # call glottoget_zenodo to download the full dataset
+    glottoget_zenodo(name = "worldatlas", dirpath = dirpath)
+  }
 
+  if (is.null(dirpath)) {
+    stop("Either set download = TRUE or provide dirpath to local data.")
+  }
+
+  # Construct path to specific GeoJSON file
+  geojson_path <- file.path(dirpath, "cldf", version, paste0(polygon_set, ".geojson"))
+
+  if (!file.exists(geojson_path)) {
+    stop("GeoJSON file not found at: ", geojson_path)
+  }
+
+  out <- sf::read_sf(geojson_path)
   return(out)
 }
