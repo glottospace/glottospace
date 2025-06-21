2d8850a0cd3802dcb7c4202972e14ef44063a9a8
meroos <meeliroose@gmail.com>
2025-06-21 12:43:40 +0300

    Add glottoget_worldatlas function to support World Atlas Zenodo data

---
+++ R/glottoget_worldatlas.R
@@ -0,0 +1,32 @@
+# 1. NEW FILE: R/glottoget_worldatlas.R
+# Save this script as R/glottoget_worldatlas.R
+
+#' Get World Atlas data from Zenodo
+#'
+#' @param version One of "contemporary" or "traditional".
+#' @param polygon_set One of "features", "languages", or "families".
+#' @param download Logical. If TRUE, downloads from Zenodo. Default is FALSE.
+#' @param dirpath Optional. Local directory path.
+#' @return sf object with spatial data.
+#' @export
+glottoget_worldatlas <- function(version = c("contemporary", "traditional"),
+                                 polygon_set = c("features", "languages", "families"),
+                                 download = FALSE,
+                                 dirpath = NULL) {
+  version <- match.arg(version)
+  polygon_set <- match.arg(polygon_set)
+
+  if (!download && is.null(dirpath)) {
+    stop("Set download = TRUE or provide dirpath.")
+  }
+
+  out <- glottoget_zenodo(
+    name = "worldatlas",
+    subfolder = file.path("cldf", version),
+    file = paste0(polygon_set, ".geojson"),
+    download = download,
+    dirpath = dirpath
+  )
+
+  return(out)
+}
